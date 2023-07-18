f06f140d070411ccebd0410d071c8d581eb80036
yangy <1467499417@qq.com>
2023-07-18 14:09:39 +0800

    1

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    //热更新方式一   直接读取
+    @Value("${pattern.dateformat}")
+    private String dateformat;
+
+    @Autowired
+    private PatternProperties patternProperties;
+
+    @GetMapping("now")
+    public String now(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(patternProperties.getDateformat()));
+    }
+    // ...略
+}
---
+++ inventory/src/main/java/com/yangy/inventory/InventoryApplication.java
@@ -0,0 +1,17 @@
+package com.yangy.inventory;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryApplication.class, args);
+
+
+        System.out.println("运行成功");
+    }
+
+}
